8b852195a9f0299dff0986237f3ac8fc19f5d880
Stephanie <stephaniiehallberg@gmail.com>
2022-09-27 17:03:29 +0200

    feat(cucumber): custom waitFor variable

--- e2e/src/step-definitions/assertions/verify-element-visibility.ts
+++ e2e/src/step-definitions/assertions/verify-element-visibility.ts
@@ -1,20 +1,7 @@
 import {Then} from '@cucumber/cucumber'
-import {expect} from '@playwright/test'
-import {ElementKey} from '../../env/global'
 import {getElementLocator} from '../../support/web-element-helper'
 import {ScenarioWorld} from "../setup/world";
-
-Then(
-    /^the "([^"]*)" should contain the text "(.*)"$/,
-    async function(this: ScenarioWorld, elementKey: ElementKey, expectedElementText: string) {
-        const {screen: {page}, globalConfig, globalVariables} = this
-
-        console.log(`the ${elementKey} should contain the text ${expectedElementText}`)
-        const elementIdentifier = getElementLocator(page, elementKey, globalVariables, globalConfig)
-        const content = await page.textContent(elementIdentifier)
-        expect(content).toBe(expectedElementText)
-    }
-)
+import {waitFor} from '../../support/wait-for-behavior'
 
 Then(
     /^the "([^"]*)" should be displayed$/,
@@ -23,7 +10,9 @@ Then(
 
         console.log(`the ${elementKey} should be displayed`)
         const elementIdentifier = getElementLocator(page, elementKey, globalVariables, globalConfig)
-        const locator = page.locator(elementIdentifier)
-        await expect(locator).toBeVisible()
+        await waitFor(async () => {
+                const isElementVisible = (await page.$(elementIdentifier)) != null
+                return isElementVisible
+        })
     }
 )
