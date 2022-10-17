23b0dd31863b2048e4e0b689014722bbe9e7752f
Stephanie <stephaniiehallberg@gmail.com>
2022-10-17 18:08:39 +0200

    feat(cucumber): added buildkite configration

--- e2e/run_tests.sh
+++ e2e/run_tests.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -ueo pipefail
+
 #environment
 env=$1
 
@@ -8,4 +12,8 @@ export COMMON_CONFIG_FILE=env/common.env
 export NODE_ENV=$env
 
 #run cucumber tests & on failure run postcucumber
-yarn run cucumber:$env --profile $tag || yarn run postcucumber
+#yarn run cucumber:$env --profile $tag || yarn run postcucumber
+if ! yarn run cucumber:$env --profile $tag; then
+  yarn run postcucumber;
+  exit 1;
+fi
